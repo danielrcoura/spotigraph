f7183861f4cb8435eb5db08c394ffc11e67a276b
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-03-23 08:03:30 -0300

    ng-model in playlist form

--- client/app/components/workspace/playlistForm.controller.js
+++ client/app/components/workspace/playlistForm.controller.js
@@ -1,11 +1,31 @@
 angular.module("spotigraph").controller("playlistFormCtrl", function ($scope) {
-    $scope.rows = [{}, {}];
-    $scope.add = function() {
-        $scope.rows.push({});
+    $scope.musics = [{name: ''}, {name: ''}];
+    $scope.add = function(index) {
+        console.log(index);
+        if (verifyField()) {
+            $scope.musics.push({name: ''});
+        }
     };
 
-    $scope.remove = function(row) {
-        var index = $scope.rows.indexOf(row);
-        $scope.rows.splice(index, 1);
+    function verifyField() {
+        $scope.musics.forEach(function (music){
+            if (music.name === '') {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    $scope.validate = function () {
+        var bool = false;
+        if ($scope.playlistField) {
+            bool = true;
+        }
+        return verifyField() && bool;
+    }
+
+    $scope.remove = function(music) {
+        var index = $scope.musics.indexOf(music);
+        $scope.musics.splice(index, 1);
     };
 });
