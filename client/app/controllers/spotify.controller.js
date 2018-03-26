eeb105dafed6b05ea4a9e73a4d6d851c8d0a675e
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-03-26 15:27:12 -0300

    seaching musics

--- client/app/controllers/spotify.controller.js
+++ client/app/controllers/spotify.controller.js
@@ -1,10 +1,22 @@
 angular.module('spotigraph').controller('spotifyCtrl', ['$scope', 'Spotify', function ($scope, Spotify) {
+    $scope.tracks;
+  
     $scope.login = function () {
-        Spotify.login().then(function (data) {
-          console.log(data);
-          alert("You are now logged in");
-        }, function () {
-          console.log('didn\'t log in');
-        })
-      };
+        Spotify.login()
+            .then(function (data) {
+                console.log(data);
+                alert("You are now logged in");
+            })
+            .catch(function () {
+                console.log('didn\'t log in');
+            })
+    };
+
+    $scope.searchArtist = function () {
+        Spotify.search($scope.searchartist, 'track').then(function (data) {
+            console.log(data.data.tracks.items);
+            $scope.tracks = data.data.tracks.items;
+        });
+    };
+
 }]);
